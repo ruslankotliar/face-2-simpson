a9fb501fc31f5b435912f8c306419f23b0b5181c
daunyl <you@example.com>
2023-08-18 23:55:43 +0200

    modelmodel

---
+++ src/app/api/_models/predict/app.py
@@ -0,0 +1,46 @@
+import os
+import torch
+import sys
+from PIL import Image
+
+from model import create_resnet
+from timeit import default_timer as timer
+from typing import Tuple, Dict
+
+cwd = os.getcwd()
+
+with open(os.path.join(cwd,"class_names.txt"), "r") as f:
+  class_names = [food_name.strip() for food_name in f.readlines()]
+
+model, transformer = create_resnet(num_classes=len(class_names))
+
+model.load_state_dict(
+    torch.load(f=os.path.join(cwd,"BEST_MODEL"),
+               map_location=torch.device("cpu"))
+)
+
+def predict(img) -> Tuple[Dict, float]:
+  start_time = timer()
+
+  img = transformer(img).unsqueeze(0)
+
+  model.eval()
+  with torch.inference_mode():
+    pred_probs = torch.softmax(model(img), dim=1)
+
+  pred_labels_and_probs = {class_names[i]: float(pred_probs[0][i]) for i in range(len(class_names))}
+
+  end_time = timer()
+  pred_time = round(end_time - start_time, 4)
+
+  return pred_labels_and_probs, pred_time
+
+
+
+img_path = sys.argv[1] # accessing args
+img = Image.open(img_path).convert('RGB')  # accessing img
+
+pred_labels_and_probs, pred_time = predict(img)
+
+print(pred_labels_and_probs, pred_time)
+sys.stdout.flush()
