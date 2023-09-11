a9824883e5456f8a88b259d90da3757335c805ba
ruslankotliar <ruslankot101@knu.ua>
2023-09-11 20:11:25 +0200

    Access env vars using os.environ

--- python-server/utils/s3_client.py
+++ python-server/utils/s3_client.py
@@ -4,24 +4,11 @@ import boto3
 
 class S3Client:
     def __init__(self):
-        # Fetching environment variables and adding error handling
-        self.AWS_ACCESS_KEY = os.environ.get("AMAZON_ACCESS_KEY")
-        if not self.AWS_ACCESS_KEY:
-            raise ValueError("Environment variable AMAZON_ACCESS_KEY is not set.")
+        self.AWS_ACCESS_KEY = os.environ["AMAZON_ACCESS_KEY"]
+        self.AWS_SECRET_KEY = os.environ["AMAZON_SECRET_KEY"]
+        self.AMAZON_REGION = os.environ["AMAZON_REGION"]
+        self.AMAZON_BUCKET = os.environ["AMAZON_BUCKET"]
 
-        self.AWS_SECRET_KEY = os.environ.get("AMAZON_SECRET_KEY")
-        if not self.AWS_SECRET_KEY:
-            raise ValueError("Environment variable AMAZON_SECRET_KEY is not set.")
-
-        self.AMAZON_REGION = os.environ.get("AMAZON_REGION")
-        if not self.AMAZON_REGION:
-            raise ValueError("Environment variable AMAZON_REGION is not set.")
-
-        self.AMAZON_BUCKET = os.environ.get("AMAZON_BUCKET")
-        if not self.AMAZON_BUCKET:
-            raise ValueError("Environment variable AMAZON_BUCKET is not set.")
-
-        # Creating the boto3 client
         self.s3 = boto3.client(
             "s3",
             aws_access_key_id=self.AWS_ACCESS_KEY,
