576a8fea020eedeba19ce1cd0bb85b87453eaf88
ruslankotliar <ruslankot101@knu.ua>
2023-09-11 19:26:47 +0200

    Fix env variables access

--- python-server/utils/s3_client.py
+++ python-server/utils/s3_client.py
@@ -1,43 +1,52 @@
 import os
 import boto3
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 class S3Client:
     def __init__(self):
-        self.AWS_ACCESS_KEY = os.getenv("AMAZON_ACCESS_KEY")
-        self.AWS_SECRET_KEY = os.getenv("AMAZON_SECRET_KEY")
-        self.AWS_REGION = os.getenv("AWS_REGION")
-        self.AWS_BUCKET = os.getenv("AWS_BUCKET")
+        # Fetching environment variables and adding error handling
+        self.AWS_ACCESS_KEY = os.environ.get("AMAZON_ACCESS_KEY")
+        if not self.AWS_ACCESS_KEY:
+            raise ValueError("Environment variable AMAZON_ACCESS_KEY is not set.")
 
+        self.AWS_SECRET_KEY = os.environ.get("AMAZON_SECRET_KEY")
+        if not self.AWS_SECRET_KEY:
+            raise ValueError("Environment variable AMAZON_SECRET_KEY is not set.")
+
+        self.AMAZON_REGION = os.environ.get("AMAZON_REGION")
+        if not self.AMAZON_REGION:
+            raise ValueError("Environment variable AMAZON_REGION is not set.")
+
+        self.AMAZON_BUCKET = os.environ.get("AMAZON_BUCKET")
+        if not self.AMAZON_BUCKET:
+            raise ValueError("Environment variable AMAZON_BUCKET is not set.")
+
+        # Creating the boto3 client
         self.s3 = boto3.client(
             "s3",
             aws_access_key_id=self.AWS_ACCESS_KEY,
             aws_secret_access_key=self.AWS_SECRET_KEY,
-            region_name=self.AWS_REGION,
+            region_name=self.AMAZON_REGION,
         )
 
     def get_s3_object(self, key):
-        obj = self.s3.get_object(Bucket=self.AWS_BUCKET, Key=key)
+        obj = self.s3.get_object(Bucket=self.AMAZON_BUCKET, Key=key)
         return obj["Body"]
 
     def put_s3_object(self, body, key, bucket=None):
         if bucket is None:
-            bucket = self.AWS_BUCKET
+            bucket = self.AMAZON_BUCKET
         obj = self.s3.put_object(Body=body, Bucket=bucket, Key=key)
         return None
 
     def get_s3_objects_list(self, bucket=None):
         if bucket is None:
-            bucket = self.AWS_BUCKET
+            bucket = self.AMAZON_BUCKET
         obj = self.s3.list_objects_v2(Bucket=bucket)
         return obj.get("Contents", [])
 
     def delete_s3_object(self, key, bucket=None):
         if bucket is None:
-            bucket = self.AWS_BUCKET
+            bucket = self.AMAZON_BUCKET
         obj = self.s3.delete_object(Bucket=bucket, Key=key)
-        return obj["DeleteMarker"]
+        return obj.get("DeleteMarker", False)
