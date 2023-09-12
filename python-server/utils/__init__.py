ef6e22bfe4dcc16f425b50037d853f7da177f083
ruslankotliar <ruslankot101@knu.ua>
2023-09-12 21:55:35 +0200

    Remove unnessary torch import

--- python-server/utils/__init__.py
+++ python-server/utils/__init__.py
@@ -1,2 +1,1 @@
 from .s3_client import S3Client
-from .set_seeds import set_seeds
--- python-server/utils/set_seeds.py
+++
@@ -1,9 +0,0 @@
-import torch
-import random
-
-def set_seeds(seed=42):
-  """
-    Set random seeds (random.seed and torch.manual_seed)
-  """
-  random.seed(seed)
-  torch.manual_seed(42)
