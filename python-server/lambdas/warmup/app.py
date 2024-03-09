b36c100b58ff99ca6f4e85996a8ec47e312fb149
ruslankotliar <ruslankot101@gmail.com>
2024-03-09 12:37:12 +0100

    Create aws warmup lambda function to reduce cold start time

--- python-server/lambdas/warmup/app.py
+++ python-server/lambdas/warmup/app.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+import os
 
 from constants import WARM_UP_KEY, WARM_UP_VALUE
 
@@ -7,39 +8,47 @@ from constants import WARM_UP_KEY, WARM_UP_VALUE
 lambda_client = boto3.client("lambda")
 
 
-def lambda_handler(event, context):
-    # Define dummy events for both lambda_predict_image and lambda_detect_face functions
-    dummy_event = {"body": json.dumps({WARM_UP_KEY: WARM_UP_VALUE})}
+def lambda_warmup(event, context):
+    try:
+        # Define dummy events for both lambda_predict_image and lambda_detect_face functions
+        dummy_event = {"body": json.dumps({WARM_UP_KEY: WARM_UP_VALUE})}
 
-    # Warm-up lambda_predict_image function
-    response_predict_image = lambda_client.invoke(
-        FunctionName="lambda_predict_image",
-        InvocationType="RequestResponse",  # Synchronous invocation
-        Payload=json.dumps(dummy_event_predict_image),
-    )
+        predict_image_arn = os.environ.get("PREDICT_IMAGE_ARN", "")
+        detect_face_arn = os.environ.get("DETECT_FACE_ARN", "")
 
-    # Warm-up lambda_detect_face function
-    response_detect_face = lambda_client.invoke(
-        FunctionName="lambda_detect_face",
-        InvocationType="RequestResponse",  # Synchronous invocation
-        Payload=json.dumps(dummy_event_detect_face),
-    )
+        # Check if ARNs are provided
+        if not predict_image_arn or not detect_face_arn:
+            raise ValueError("One or both ARNs are missing.")
 
-    # Check if both invocations were successful
-    if (
-        response_predict_image["StatusCode"] == 200
-        and response_detect_face["StatusCode"] == 200
-    ):
-        return {
-            "statusCode": 200,
-            "body": json.dumps(
-                "Warm-up successful for lambda_predict_image and lambda_detect_face"
-            ),
-        }
-    else:
+        response_predict_image = lambda_client.invoke(
+            FunctionName=predict_image_arn,
+            InvocationType="Event",  # Asynchronous invocation
+            Payload=json.dumps(dummy_event),
+        )
+
+        response_detect_face = lambda_client.invoke(
+            FunctionName=detect_face_arn,
+            InvocationType="Event",  # Asynchronous invocation
+            Payload=json.dumps(dummy_event),
+        )
+
+        # Check if both invocations were successful
+        if (
+            response_predict_image["StatusCode"] == 202
+            and response_detect_face["StatusCode"] == 202
+        ):
+            return {
+                "statusCode": 200,
+                "body": json.dumps(
+                    "Warm-up successful for lambda_predict_image and lambda_detect_face"
+                ),
+            }
+        else:
+            raise Exception(
+                "One or both lambda functions failed to respond with the expected status code."
+            )
+    except Exception as e:
         return {
             "statusCode": 500,
-            "body": json.dumps(
-                "Warm-up failed for lambda_predict_image and lambda_detect_face"
-            ),
+            "body": json.dumps(f"Warm-up failed: {str(e)}"),
         }
